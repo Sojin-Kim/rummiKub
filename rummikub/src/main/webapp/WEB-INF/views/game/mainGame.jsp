9432d7f86eb523a7fad1b2fb34944e7e08e96dbb
sojin <sojin@DESKTOP-VLASB4E>
2023-06-28 19:39:04 +0900

    2023-06-28 김소진 : 메인게임화면 시작함수 추가

--- rummikub/src/main/webapp/WEB-INF/views/game/mainGame.jsp
+++ rummikub/src/main/webapp/WEB-INF/views/game/mainGame.jsp
@@ -9,26 +9,52 @@
 <meta charset="UTF-8">
 <title>rummiKub</title>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script type="text/javascript" src="./resources/js/common.js"></script>
+<script type="text/javascript" src="../resources/js/common.js"></script>
+<script type="text/javascript" src="../resources/js/game/mainGame.js"></script>
 <link rel="stylesheet" href="${pageContext.request.contextPath}/resources/css/common.css">
+<link rel="stylesheet" href="${pageContext.request.contextPath}/resources/css/game/mainGame.css">
 </head>
 <!-- 
   메인 게임 화면
-  2023/06/23 ksj 추가
+  2023/06/28 ksj 추가
 -->
-<style>
-#loadingDiv{
-	position: relative;
-}
-
-#loadingImg{
-	position: absolute;
-    transform: translate(10%, 0%);
-} /* 이미지 중앙 정렬 */
-
-</style>
 <body>
-	<div id="loadingDiv">
+	<input type="button" value="시작하기" id="startBtn" onclick="start()">
+	
+	<main>
+	  <table id="mainGrid">
+	    
+	  </table>
+	</main>
+	<div id="prifile">
+	  
 	</div>
+	<div id="privateGrid">	<!-- 내꺼 빼고 히든 시킬 예정 :  -->
+		player1
+		<table id="privateTable1">
+		
+		</table>
+		player2
+		<table id="privateTable2">
+		
+		</table>
+		player3
+		<table id="privateTable3">
+		
+		</table>
+		player4
+		<table id="privateTable4">
+		
+		</table>
+	</div>
+	
+	<div id="card">
+	  <div id="jokercard">
+	    <div class="blackcard card cardPack" id="card105">J</div>
+	    <div class="redcard card cardPack" id="card106">J</div>
+	  </div>
+	</div>
+	
 </body>
+
 </html>
--- rummikub/src/main/webapp/resources/js/game/mainGame.js
+++ rummikub/src/main/webapp/resources/js/game/mainGame.js
@@ -2,3 +2,105 @@
  * 메인 게임 화면
  * 2023/06/23 ksj 추가
  */
+$(document).ready(function(){
+	var card = document.getElementsByClassName("card");
+	
+	console.log(card);
+
+	// 게임판 만들기
+	var gridStr = "";
+	for(var i=0 ; i < 7 ; i++){
+		gridStr += "<tr>\n";
+		for(var j=0 ; j < 21 ; j++){
+			gridStr += "<td></td>\n";
+		}
+		gridStr += "</tr>\n";
+	}
+	$("#mainGrid").append(gridStr);
+	
+	// 개인 타일 판 만들기
+	var pGridStr = "";
+	for(var i=0 ; i < 2 ; i++){
+		pGridStr += "<tr>\n";
+		for(var j=0 ; j < 10 ; j++){
+			pGridStr += "<td class='privateTd"+(i*10+j)+"'></td>\n";
+		}
+		pGridStr += "</tr>\n";
+	}
+	$("#privateTable1").append(pGridStr);
+	$("#privateTable2").append(pGridStr);
+	$("#privateTable3").append(pGridStr);
+	$("#privateTable4").append(pGridStr);
+	
+	
+	// 전체 카드팩 만들기 -> 더 괜찮은 방법 생각해보기
+	var cardStr = "";
+	cardStr += '<div id="blackcard">';
+	for(var i=1 ; i <= 13 ; i++){
+		cardStr += '<div class="blackcard card cardPack" id="card'+ (i*2-1) +'">'+i+'</div>\n';
+		cardStr += '<div class="blackcard card cardPack" id="card'+ (i*2) +'">'+i+'</div>\n';
+	}
+	cardStr += '</div>';
+	cardStr += '<div id="redcard">';
+	for(var i=1 ; i <= 13 ; i++){
+		cardStr += '<div class="redcard card cardPack" id="card'+ (i*2+25) +'">'+i+'</div>\n';
+		cardStr += '<div class="redcard card cardPack" id="card'+ (i*2+26) +'">'+i+'</div>\n';
+	}
+	cardStr += '</div>';
+	cardStr += '<div id="bluecard">';
+	for(var i=1 ; i <= 13 ; i++){
+		cardStr += '<div class="bluecard card cardPack" id="card'+ (i*2+51) +'">'+i+'</div>\n';
+		cardStr += '<div class="bluecard card cardPack" id="card'+ (i*2+52) +'">'+i+'</div>\n';
+	}
+	cardStr += '</div>';
+	cardStr += '<div id="redcard">';
+	for(var i=1 ; i <= 13 ; i++){
+		cardStr += '<div class="yellowcard card cardPack" id="card'+ (i*2+77) +'">'+i+'</div>\n';
+		cardStr += '<div class="yellowcard card cardPack" id="card'+ (i*2+78) +'">'+i+'</div>\n';
+	}
+	cardStr += '</div>';
+	$("#card").append(cardStr);
+	
+});
+
+function start(){
+	$("#startBtn").hide();
+	var randomNum = Math.floor(Math.random() * 106) + 1;
+	console.log(randomNum);
+	
+	// 14장씩 나눠주기
+	for(var p = 0; p < playerCard.length ; p++){	// playerCard.length 플레이어 숫자로 변경
+		while(true){	// 무한루프
+			var randomNum = Math.floor(Math.random() * 106) + 1;
+			var card = $("#card" + randomNum);
+			console.log(card.attr('class'));
+			if(card.attr('class').match("cardPack")){	// cardPack 클래스 있는지 확인
+				console.log("카드팩 안에 존재");
+				card.attr('class',$("#card1").attr('class').replace(' cardPack',''));
+				playerCard[p][playerCard[p].length] = randomNum;
+			}else{
+				console.log("카드팩 안에 미존재");
+			}
+			if(playerCard[p].length == 14){		// 14장일 경우 멈추기
+				break;
+			}
+		}
+	}
+
+	// 화면에서 애니메이션 추가
+	for(var p = 0; p < playerCard.length ; p++){	// 플레이어
+		var tiles = $("#privateTable"+(p+1));
+		console.log(tiles);
+		for(var c = 0; c < playerCard[p].length ; c++){	// 가지고 있는 카드 번호
+			var td = tiles.find(".privateTd"+c);
+			console.log(playerCard[p][c]);
+			var card = $("#card"+playerCard[p][c]);
+			console.log(card);
+			td.append(card);
+			//card.remove();
+		}
+	}
+	console.log(playerCard);
+}
+
+var playerCard = [[],[],[],[]];
